85aece2c0ab64fe3e4b6c339fc3629cd3585d37f
VadimKorobec <_s_e_t__@ukr.net>
2022-11-13 14:25:47 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,11 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix';
 
+const refs = {
+  inputEl: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+};
+
 const options = {
   enableTime: true,
   time_24hr: true,
